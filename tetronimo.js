4fdbd3d63004a718c5ab04a86b74ca50640af044
Nick Mai <nick.mai@outlook.com>
2015-12-17 17:46:00 -0800

    made a tetris

--- tetronimo.js
+++ tetronimo.js
@@ -1,14 +1,14 @@
 'use strict'
 let array2d =  require('array2d')
 
-let grid = new Array(20)
-grid = grid.map(function () {
-	let row = []
-	for (let i = 0; i < 10; i++) {
-		console.log('hello')
-		row.push(0)
-	}
-	return row
-})
+let grid = []
+
+for (let i = 0; i < 20; i++) {
+  let row = []
+  for (let j = 0; j < 10; j++) {
+    row.push(0)
+  }
+  grid.push(row)
+}
 
 console.log(grid)
