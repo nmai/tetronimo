7982141d5076e637c5bd4b7133061eb8509b6c89
Ryan <ryan.mai@mail.com>
2015-12-17 17:37:52 -0800

    make grid

--- tetronimo.js
+++ tetronimo.js
@@ -1,3 +1,14 @@
 'use strict'
+let array2d =  require('array2d')
 
+let grid = new Array(20)
+grid = grid.map(function () {
+	let row = []
+	for (let i = 0; i < 10; i++) {
+		console.log('hello')
+		row.push(0)
+	}
+	return row
+})
 
+console.log(grid)
