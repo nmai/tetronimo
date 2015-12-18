fa4d1035a3e260d6e5608180084ae40738a9c5a7
Nick Mai <nick.mai@outlook.com>
2015-12-17 21:11:06 -0800

    add paperjs as dep, add grid.js for grid rendering

--- tetronimo.js
+++ tetronimo.js
@@ -6,6 +6,13 @@ let grid = Array2D.build(10, 20, 0)
 
 console.log(grid)
 
+let COLORS = {
+  0: 'none',
+  1: 'red',
+  2: 'blue',
+  3: 'green'
+}
+
 let I = [
   [0,0,0,0],
   [1,1,1,1],
@@ -16,39 +23,52 @@ let I = [
 let L = [
   [0,0,1],
   [1,1,1],
-  [0,0,0],
+  [0,0,0]
 ]
 
 let J = [
   [1,0,0],
   [1,1,1],
-  [0,0,0],
+  [0,0,0]
 ]
 
 let S = [
   [1,1,0],
   [0,1,1],
   [0,0,0]
- ]
+]
 
- let Z = [
+let Z = [
   [0,1,1],
   [1,1,0],
   [0,0,0]
- ]
+]
 
- let O = [
+let O = [
   [1,1],
-  [1,1],
- ]
+  [1,1]
+]
 
- let T = [
+let T = [
   [0,1,0],
   [1,1,1],
   [0,0,0]
- ]
+]
 
+let def = function () {
+  this.x = 1
+  this.y = 2
+}
 
+let a = new def()
+
+a.x = 3
+
+let b = new def()
+
+b.x = 4
+
+console.log(a.x + ' ' + b.x)
 
 /*
 debounce step only if collision in next frame
