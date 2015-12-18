bf2ddbe7ec23d7573f37ad0b42fa06fd138a6cd6
Nick Mai <nick.mai@outlook.com>
2015-12-17 18:21:04 -0800

    use Array2D for grid initialization

--- tetronimo.js
+++ tetronimo.js
@@ -1,14 +1,24 @@
 'use strict'
-let array2d =  require('array2d')
 
-let grid = []
+let Array2D =  require('array2d')
 
-for (let i = 0; i < 20; i++) {
-  let row = []
-  for (let j = 0; j < 10; j++) {
-    row.push(0)
-  }
-  grid.push(row)
-}
+let grid = Array2D.build(10, 20, 0)
 
 console.log(grid)
+
+let I = [
+  [0,0,0,0],
+  [1,1,1,1],
+  [0,0,0,0],
+  [0,0,0,0]
+]
+
+console.log(I)
+
+/*
+debounce step only if collision in next frame
+also check collision for horizontal 
+
+1. step event fired: "drop" and check for collision
+2. rotation: 
+*/
