49fc2c74fd1305003105d75919346120125d2787
Nick Mai <nick.mai@outlook.com>
2015-12-18 23:08:26 -0800

    stash paper.js stuff, add canvas examples, weak improvement to array formats

--- tetronimo.js
+++ tetronimo.js
@@ -6,54 +6,49 @@ let grid = Array2D.build(10, 20, 0)
 
 console.log(grid)
 
-let COLORS = {
+let COLOR = {
   0: 'none',
   1: 'red',
   2: 'blue',
   3: 'green'
 }
 
-let I = [
-  [0,0,0,0],
-  [1,1,1,1],
-  [0,0,0,0],
-  [0,0,0,0]
-]
+let PIECE = {  
+  I: [ [0,0,0,0],
+       [1,1,1,1],
+       [0,0,0,0],
+       [0,0,0,0]
+     ],
 
-let L = [
-  [0,0,1],
-  [1,1,1],
-  [0,0,0]
-]
+  L: [ [0,0,1],
+       [1,1,1],
+       [0,0,0]
+     ],
 
-let J = [
-  [1,0,0],
-  [1,1,1],
-  [0,0,0]
-]
+  J: [ [1,0,0],
+       [1,1,1],
+       [0,0,0]
+     ],
 
-let S = [
-  [1,1,0],
-  [0,1,1],
-  [0,0,0]
-]
+  S: [ [1,1,0],
+       [0,1,1],
+       [0,0,0]
+     ],
 
-let Z = [
-  [0,1,1],
-  [1,1,0],
-  [0,0,0]
-]
+  Z: [ [0,1,1],
+       [1,1,0],
+       [0,0,0]
+     ],
 
-let O = [
-  [1,1],
-  [1,1]
-]
+  O: [ [1,1],
+       [1,1]
+     ],
 
-let T = [
-  [0,1,0],
-  [1,1,1],
-  [0,0,0]
-]
+  T: [ [0,1,0],
+       [1,1,1],
+       [0,0,0] 
+     ]
+}
 
 let def = function () {
   this.x = 1
@@ -76,4 +71,23 @@ also check collision for horizontal
 
 1. step event fired: "drop" and check for collision
 2. rotation: 
+*/
+
+/*
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.fillRect(20,20,150,100);
+*/
+
+/*
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillRect(25,25,100,100);
+    ctx.clearRect(45,45,60,60);
+    ctx.strokeRect(50,50,50,50);
+  }
+}
 */
