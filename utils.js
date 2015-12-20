e79e2e721667b40a4eb0a48a6da015ddf5a1b0be
Nick Mai <nick.mai@outlook.com>
2015-12-19 22:56:11 -0800

    add shuffle method

--- utils.js
+++ utils.js
@@ -14,3 +14,13 @@ function debounce(callback, delay) {
       timer = setTimeout(callback, delay)
   }
 }
+
+/**
+ * Knuth-Fisher-Yates Shuffle Algorithm
+ *
+ * Note: shuffles in place, so the original array gets modified.
+ */
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
